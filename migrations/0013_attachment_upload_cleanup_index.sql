48fb089b7c651bae523624bae50f839cdb30a93a
Park Jangwon <vim@kakao.com>
2026-05-04 01:24:08 +0900

    feat: index stale attachment upload cleanup

---
+++ migrations/0013_attachment_upload_cleanup_index.sql
@@ -0,0 +1,7 @@
+-- +goose Up
+CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_attachments_uploading_created_at
+  ON attachments (created_at ASC, id ASC)
+  WHERE status = 'uploading';
+
+-- +goose Down
+DROP INDEX CONCURRENTLY IF EXISTS idx_attachments_uploading_created_at;
