cbf5f202c953df2e3b60110daf5aa5a0bb374218
parkjangwon <vim@kakao.com>
2026-05-04 18:05:33 +0900

    feat: deduplicate api usage export artifacts

---
+++ migrations/0035_api_usage_export_artifact_uniqueness.sql
@@ -0,0 +1,5 @@
+CREATE UNIQUE INDEX IF NOT EXISTS idx_api_usage_export_artifacts_batch_object_key
+  ON api_usage_export_artifacts (batch_id, object_key);
+
+CREATE UNIQUE INDEX IF NOT EXISTS idx_api_usage_export_artifacts_batch_sha256
+  ON api_usage_export_artifacts (batch_id, sha256_hex);
